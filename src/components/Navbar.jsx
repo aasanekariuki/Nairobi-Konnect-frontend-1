6834708c17f986066b87a5c77c2174729582c7e0
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-08-16 16:38:37 +0300

    Logout added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faEnvelope, faCogs, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faEnvelope, faCogs, faSignInAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 import logoutIcon from '../assets/logout.png'; // Adjust this path if necessary
-import './styles/Navbar.css'
-import { faUser, faHome, faEnvelope, faCogs, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import './styles/Navbar.css';
 
 const Navbar = () => {
@@ -43,6 +41,7 @@ const Navbar = () => {
 
     const handleLogout = () => {
         localStorage.removeItem('user');
+        setUser(null);
         navigate('/login');
     };
 
@@ -68,43 +67,49 @@ const Navbar = () => {
                             <FontAwesomeIcon icon={faEnvelope} className="icon" /> Contact
                         </a>
                     </li>
-                    <li>
-                        <Link to="/user" className="navbar-item">
-                            <FontAwesomeIcon icon={faUser} className="icon" /> User
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/login" className="navbar-item login-button">
-                            <FontAwesomeIcon icon={faSignInAlt} className="icon" /> Login
-                        </Link>
-                    </li>
-                    <li>
-                        <div onClick={handleLogout} className="cursor-pointer">
-                       <img src={logoutIcon} alt="Logout" className="h-11 w-11 ml-4 transition-transform duration-200 hover:scale-110" />
-                    </div>
-                    </li>
+                    {user ? (
+                        <>
+                            <li>
+                                <Link to="/user" className="navbar-item">
+                                    <FontAwesomeIcon icon={faUser} className="icon" /> {user.name}
+                                </Link>
+                            </li>
+                            <li>
+                                <div onClick={handleLogout} className="cursor-pointer">
+                                    <img src={logoutIcon} alt="Logout" className="h-11 w-11 ml-4 transition-transform duration-200 hover:scale-110" />
+                                </div>
+                            </li>
+                        </>
+                    ) : (
+                        <li>
+                            <Link to="/login" className="navbar-item login-button">
+                                <FontAwesomeIcon icon={faSignInAlt} className="icon" /> Login
+                            </Link>
+                        </li>
+                    )}
                 </ul>
                 
                 {/* Profile Section */}
-                <div className="flex items-center space-x-2 cursor-pointer">
-                    <div className="flex flex-col items-center mr-24" onClick={goToProfile}>
-                        {user.profilePhoto ? (
-                            <img
-                                src={user.profilePhoto}
-                                alt="Profile"
-                                className="h-12 w-12 rounded-full object-cover border border-gray-300"
-                            />
-                        ) : (
-                            <div className="bg-gray-200 h-12 w-12 rounded-full flex items-center justify-center text-gray-700 text-lg border border-gray-300">
+                {user && (
+                    <div className="flex items-center space-x-2 cursor-pointer">
+                        <div className="flex flex-col items-center mr-24" onClick={goToProfile}>
+                            {user.profilePhoto ? (
+                                <img
+                                    src={user.profilePhoto}
+                                    alt="Profile"
+                                    className="h-12 w-12 rounded-full object-cover border border-gray-300"
+                                />
+                            ) : (
+                                <div className="bg-gray-200 h-12 w-12 rounded-full flex items-center justify-center text-gray-700 text-lg border border-gray-300">
+                                    {getInitials(user.name)}
+                                </div>
+                            )}
+                            <div className="text-sm font-medium text-gray-700 mt-1">
                                 {getInitials(user.name)}
                             </div>
-                        )}
-                        <div className="text-sm font-medium text-gray-700 mt-1">
-                            {getInitials(user.name)}
                         </div>
                     </div>
-                    
-                </div>
+                )}
             </div>
         </nav>
     );
